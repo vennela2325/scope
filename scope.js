62135b1db3953eaa76306b0e96da3058820762c5
vennela2325 <vennelakothoju@gmail.com>
2024-12-20 22:34:51 +0530

    scope.js code uncommented

--- scope.js
+++ scope.js
@@ -1,85 +1,85 @@
-// var x=200;
-// function a()
-// {    var x=100;
-//      console.log(x);
-//  }
+var x=200;
+function a()
+{    var x=100;
+     console.log(x);
+ }
 
-//  a()
-//  console.log(x,"the values are")
-//  function b(){
-//  function x()
-// {
-//      function hello() {
-//  console.log(100 +z);  }   
-//    hello()}
-//     x()
-//  }
-//  var z=100;
-//  console.log(z)
-// b()
+ a()
+ console.log(x,"the values are")
+ function b(){
+ function x()
+{
+     function hello() {
+ console.log(100 +z);  }   
+   hello()}
+    x()
+ }
+ var z=100;
+ console.log(z)
+b()
 
-//  function c(){
-//      function x()
-//     {
-//        function hello() {
-//       console.log(100 +z);  }   
-//       hello()}
+ function c(){
+     function x()
+    {
+       function hello() {
+      console.log(100 +z);  }   
+      hello()}
     
-//          x()
-//    }
-//  c()
-//     var z=100;
-// console.log(z)
+         x()
+   }
+ c()
+    var z=100;
+console.log(z)
 
 
-// var userinput=prompt("enter the digit")
-// var max = userinput[0];
-// for(i=0;i<userinput.length;i++){
-//     if(max<userinput[i]){
-//         max = userinput[i];
-//     }
-// }
-// console.log(max,"is highest number");
+var userinput=prompt("enter the digit")
+var max = userinput[0];
+for(i=0;i<userinput.length;i++){
+    if(max<userinput[i]){
+        max = userinput[i];
+    }
+}
+console.log(max,"is highest number");
 
-// var userinput=prompt("enter the digit")
-// var min = userinput[0];
-// var max = userinput[0];
-// for(i=0;i<userinput.length;i++){
-//     if(max<userinput[i]){
-//         max = userinput[i];
-//     }
-//     else{
-//         min=userinput[i];
-//     }
-// }
-// console.log(max,"is highest number");
-// console.log(min,"is highest number");
+var userinput=prompt("enter the digit")
+var min = userinput[0];
+var max = userinput[0];
+for(i=0;i<userinput.length;i++){
+    if(max<userinput[i]){
+        max = userinput[i];
+    }
+    else{
+        min=userinput[i];
+    }
+}
+console.log(max,"is highest number");
+console.log(min,"is highest number");
 
-// var userinput=prompt("enter the digit")
-// var min = userinput[0];
-// var max = userinput[0];
-// for(i=0;i<userinput.length;i++){
-//     if(max<userinput[i]){
-//         max = userinput[i];
-//     }
-//     if(min>userinput[i])
-//         {
-//         min=userinput[i];
-//     }
-// }
-// console.log(max,"is highest number");
-// console.log(min,"is highest number");
-// var userinput=prompt("enter the digit")
-// var min = userinput[0];
-// var max = userinput[0];
-// for(i=0;i<userinput.length;i++){
-//     if(max<userinput[i]){
-//         max = userinput[i];
-//     }
-//     if(min>userinput[i])
-//         {
-//         min=userinput[i];
-//     }
-// }
-// console.log(max,"is highest number");
-// console.log(min,"is highest number");
+var userinput=prompt("enter the digit")
+var min = userinput[0];
+var max = userinput[0];
+for(i=0;i<userinput.length;i++){
+    if(max<userinput[i]){
+        max = userinput[i];
+    }
+    if(min>userinput[i])
+        {
+        min=userinput[i];
+    }
+}
+console.log(max,"is highest number");
+console.log(min,"is highest number");
+var userinput=prompt("enter the digit")
+var min = userinput[0];
+var max = userinput[0];
+for(i=0;i<userinput.length;i++){
+    if(max<userinput[i]){
+        max = userinput[i];
+    }
+    if(min>userinput[i])
+        {
+        min=userinput[i];
+    }
+}
+console.log(max,"is highest number");
+console.log(min,"is highest number");
